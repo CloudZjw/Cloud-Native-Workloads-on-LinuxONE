cd376b96bd3e696c68c388feb9cf2967da2b8778
aWei1999 <Cloudback198@163.com>
2019-05-31 13:45:18 +0800

    1.7

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -1,19 +1,19 @@
 var mainController = angular.module('Controller', []);
 
 
-	mainController.controller('Register', ['$scope', '$http', 'Users', function ($scope, $http, Users) {
-		$scope.fromUserData = {};
-		$scope.registering = false;
+mainController.controller('Register', ['$scope', '$http', 'Users', function ($scope, $http, Users) {
+	$scope.fromUserData = {};
+	$scope.registering = false;
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
 		Users.get()
-			.success(function (data) {
-				console.log(data);
-				$scope.users = data;
-				$scope.registering = false;
-			});
+		.success(function (data) {
+			console.log(data);
+			$scope.users = data;
+			$scope.registering = false;
+		});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
@@ -37,93 +37,94 @@ var mainController = angular.module('Controller', []);
 					});
 				}	
 			}
+
+			isDigitOrLetter = function (s) {
+				var re = /^[0-9a-zA-Z]*$/g;
+				if (!re.test(s))
+					return false;
+				return true;
+			};
+
+			isEmail = function (s) {
+				var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+				if (!pattern.test(s))
+					return false;
+				return true;
+			}
+
+			$scope.userNameFormmatRight = function () {
+				if ('user_name' in $scope.fromUserData && $scope.fromUserData.user_name) {
+					return $scope.userNameValid();
+				}
+				return true;
+			};
+
+			$scope.userNameValid = function () {
+				if (!'user_name' in $scope.fromUserData || !$scope.fromUserData.user_name)
+					return false;
+				var user_name = $scope.fromUserData.user_name;
+				return isDigitOrLetter(user_name) && user_name.length >= 6 && user_name.length <= 15;
+			};
+
+			$scope.codeFormmatRight = function () {
+				if ('code' in $scope.fromUserData && $scope.fromUserData.code) {
+					var code = $scope.fromUserData.code;
+					return isDigitOrLetter(code) && code.length >= 6;
+				}
+				return true;
+			};
+
+			$scope.emailFormmatRight = function () {
+				if ('email' in $scope.fromUserData && $scope.fromUserData.email) {
+					var email = $scope.fromUserData.email;
+					return isEmail(email);
+				}
+				return true;
+			};
+
+			emailValid = function () {
+				if (!'email' in $scope.fromUserData || !$scope.fromUserData.email)
+					return false;
+				return isEmail($scope.fromUserData.email);
+			};
+
+
+			codeValid = function () {
+				if (!'code' in $scope.fromUserData)
+					return false;
+				else
+					return $scope.fromUserData.code && $scope.codeAgain && $scope.codeFormmatRight() && $scope.fromUserData.code == $scope.codeAgain;
+			};
+
+			$scope.allRight = function () {
+				var allRight = $scope.userNameValid() && codeValid() && emailValid();
+				console.log(allRight);
+				return allRight;
+			};
+
+
+			$scope.submit = function () {
+				$scope.createUser();
+			};
+
 		}]);
 
-		isDigitOrLetter = function (s) {
-			var re = /^[0-9a-zA-Z]*$/g;
-			if (!re.test(s))
-				return false;
-			return true;
-		};
 
-		isEmail = function (s) {
-			var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-			if (!pattern.test(s))
-				return false;
-			return true;
-		}
-
-		$scope.userNameFormmatRight = function () {
-			if ('user_name' in $scope.fromUserData && $scope.fromUserData.user_name) {
-				return $scope.userNameValid();
-			}
-			return true;
-		};
-
-		$scope.userNameValid = function () {
-			if (!'user_name' in $scope.fromUserData || !$scope.fromUserData.user_name)
-				return false;
-			var user_name = $scope.fromUserData.user_name;
-			return isDigitOrLetter(user_name) && user_name.length >= 6 && user_name.length <= 15;
-		};
-
-		$scope.codeFormmatRight = function () {
-			if ('code' in $scope.fromUserData && $scope.fromUserData.code) {
-				var code = $scope.fromUserData.code;
-				return isDigitOrLetter(code) && code.length >= 6;
-			}
-			return true;
-		};
-
-		$scope.emailFormmatRight = function () {
-			if ('email' in $scope.fromUserData && $scope.fromUserData.email) {
-				var email = $scope.fromUserData.email;
-				return isEmail(email);
-			}
-			return true;
-		};
-
-		emailValid = function () {
-			if (!'email' in $scope.fromUserData || !$scope.fromUserData.email)
-				return false;
-			return isEmail($scope.fromUserData.email);
-		};
-
-
-		codeValid = function () {
-			if (!'code' in $scope.fromUserData)
-				return false;
-			else
-				return $scope.fromUserData.code && $scope.codeAgain && $scope.codeFormmatRight() && $scope.fromUserData.code == $scope.codeAgain;
-		};
-
-		$scope.allRight = function () {
-			var allRight = $scope.userNameValid() && codeValid() && emailValid();
-			console.log(allRight);
-			return allRight;
-		};
-
-
-		$scope.submit = function () {
-			$scope.createUser();
-		};
-
-
-	mainController.controller('wishController', ['$scope', '$http', 'Wishes', function ($scope, $http, Wishes) {
-		$scope.wishData = {};
-		$scope.wishing = false;
-		$scope.bonusList = [1, 2, 3];
-		console.log($scope.wishData);
+mainController.controller('wishController', ['$scope', '$http', 'Wishes', function ($scope, $http, Wishes) {
+	$scope.wishData = {};
+	$scope.wishing = false;
+	$scope.bonusList = [1, 2, 3];
+	console.log($scope.wishData);
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
 		Wishes.get()
-			.success(function (data) {
-				console.log(data);
-				$scope.wishes = data;
-				$scope.wishing = false;
-			});
+		.success(function (data) {
+			console.log(data);
+			$scope.wishes = data;
+			$scope.wishing = false;
+		});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
@@ -147,7 +148,7 @@ var mainController = angular.module('Controller', []);
 						$scope.wishData = {}; // clear the form so our user is ready to enter another
 
 					});
-			}
+				}
 
-		}
-	}]);
+			}
+		}]);
